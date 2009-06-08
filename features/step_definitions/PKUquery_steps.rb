8d68670166cc0df610beeb6c56b9887456554d97
Rafael Crivellari Saliba Schouery <schouery@gmail.com>
2009-06-08 20:09:37 -0300

    Working on PKU

--- features/step_definitions/PKUquery_steps.rb
+++ features/step_definitions/PKUquery_steps.rb
@@ -2,21 +2,10 @@ Given /^user = "([^\"]*)"$/ do |user|
   @user = user
 end
 
-Given /^password = "([^\"]*)"$/ do |password|
-  @password = password
-end
-
 When /^I ask for submissions$/ do
-  @pku = PKU.new(@user, @password)
+  
 end
 
 Then /^I should get the submissions$/ do
-  submissions = @pku.submissions
-  submissions[0].should =~ /5137643/
-  submissions[0].should =~ /rtreino/
-  submissions[0].should =~ /1000/
-  submissions[0].should =~ /Wrong Answer/
-  submissions[0].should =~ /GCC/
-  submissions[0].should =~ /30B/
-  submissions[0].should =~ /2009-05-12 06:49:32/
+  PKU.submissions_for(@user).should =~ /5137643\s+rtreino\s+1000\s+Wrong\s+Answer\s+GCC\s+30B\s+2009-05-12\s+06:49:32/
 end
--- spec/lib/pku_spec.rb
+++ spec/lib/pku_spec.rb
@@ -1,25 +1,16 @@
 require 'lib/pku'
+require 'net/http'
+require 'rubygems'
+require 'hpricot'
 
 describe PKU do
 
-  before(:each) do
-    @pku = PKU.new("rtreino", "password")
-  end
-
-  it "should save username" do
-    @pku.should respond_to :username
-    @pku.username.should == "rtreino"
+  it "should request for the submissions for the user" do
+    PKU.should respond_to :submissions_for
+    html = open(File.dirname(__FILE__) + "/PKU_examples/one_submission.html")
+    PKU.should_receive(:open).with("http://acm.pku.edu.cn/JudgeOnline/status?user_id=rtreino").and_return(html)
+    PKU.should_receive(:Hpricot).with(html)
+    PKU.submissions_for("rtreino").should =~ /5137643\s+rtreino\s+1000\s+Wrong Answer\s+GCC\s+30B\s+2009-05-12\s+06:49:32/
   end
   
-  it "should save password" do
-    @pku.should respond_to :password
-    @pku.password.should == "password"
-  end
-
-  it "should get the submissions" do
-    @pku.should respond_to :submissions
-    PKUParser.should_receive(:submissions).and_return(["sub1", "sub2"])
-    @pku.submissions.should == ["sub1", "sub2"]
-  end
-
 end
