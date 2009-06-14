264d9abe09738ed6338825564e87d092951c7b33
Rafael Crivellari Saliba Schouery <schouery@gmail.com>
2009-06-14 11:42:30 -0300

    Solving merging problems. We lost the pku_spec, must be rewrited.

--- spec/pku_spec.rb
+++ spec/pku_spec.rb
@@ -3,22 +3,4 @@ require 'lib/pku'
 
 describe PKU do
 
-  before(:each) do
-    @pku = PKU.new("rtreino", "password")
-  end
-
-  it "should save username" do
-    @pku.should respond_to :username
-    @pku.username.should == "rtreino"
-  end
-  
-  it "should save password" do
-    @pku.should respond_to :password
-    @pku.password.should == "password"
-  end
-
-  it "should get the submissions" do
-    @pku.should respond_to :submissions
-  end
-
 end
